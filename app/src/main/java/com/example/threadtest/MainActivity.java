11863a3716f908ba6fe4162cc1fb4e0a852dd541
Scibi S V <scibi.s.v@gmail.com>
2020-12-18 12:42:06 +0530

    my code

---
+++ app/src/main/java/com/example/threadtest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.threadtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+}
